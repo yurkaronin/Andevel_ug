085a64e53938879367c7aed61d87776c3c8edb88
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-07-26 13:09:51 +0300

    Продолжаю разметку и стилизацию первого экрана

--- js/swiper-sliders.js
+++ js/swiper-sliders.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  if (document.querySelector(".js-slider-photo")) {
-    const swiper = new Swiper(".js-slider-photo", {
+  if (document.querySelector(".js-hero-slider-photo")) {
+    const swiper = new Swiper(".js-hero-slider-photo", {
       // Optional parameters
       loop: true,
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".js-hero-slider-text")) {
+    const swiper = new Swiper(".js-hero-slider-text", {
+      // Optional parameters
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: ".js-hero-sliders .swiper-pagination",
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".js-hero-sliders .swiper-button-next",
+        prevEl: ".js-hero-sliders .swiper-button-prev",
+      },
+    });
+  }
+});
+
