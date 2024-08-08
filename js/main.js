81fd9cc717de1de7f5e07f2b2051be39aea626cc
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-08-08 16:04:40 +0300

    вторая секция с табами и анонсами товаров

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  // new SimpleBar(document.querySelector(".tabs-list"));
 });
-
-
 
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
 // получить в консоли элемент, по которому кликнули
-document.addEventListener('click', e => console.log(e.target));
+document.addEventListener("click", (e) => console.log(e.target));
--- js/swiper-sliders.js
+++ js/swiper-sliders.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
       loop: true,
     });
   }
-});
 
-document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".js-hero-slider-text")) {
     const swiper = new Swiper(".js-hero-slider-text", {
       // Optional parameters
@@ -25,5 +23,43 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     });
   }
-});
 
+  // скрипт инициализации мини-слайдеров в анонсах
+  if (document.querySelector(".announce-card__slider")) {
+    const sliderCards = document.querySelectorAll(".announce-card");
+
+    sliderCards.forEach((card, index) => {
+      const sliderContainer = card.querySelector(".js-announce-card-slider");
+
+      if (sliderContainer) {
+        sliderContainer.classList.add(`announce-card-gallery-${index}`);
+        const swiperTest = new Swiper(`.announce-card-gallery-${index}`, {});
+
+        const navItems = card.querySelectorAll(
+          ".announce-card__navigation-item"
+        );
+
+        navItems.forEach((item, i) => {
+          item.setAttribute("data-number", i);
+        });
+
+        card
+          .querySelector(".announce-card__navigation")
+          .addEventListener("mouseover", (event) => {
+            if (
+              event.target.classList.contains("announce-card__navigation-item")
+            ) {
+              navItems.forEach((elem) => {
+                elem.classList.remove("announce-card__navigation-item--active");
+              });
+
+              event.target.classList.add(
+                "announce-card__navigation-item--active"
+              );
+              swiperTest.slideTo(event.target.getAttribute("data-number"));
+            }
+          });
+      }
+    });
+  }
+});
